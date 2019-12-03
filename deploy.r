2473f745a1fef2681194be62e9049334523318e2
ischerr <iainscherr@gmail.com>
2019-12-03 15:27:09 +0000

    Update deploy.r

--- deploy.r
+++ deploy.r
@@ -1,9 +1,4 @@
 library(rsconnect)
-
-# Print a list of app dependencies. Libraries need to be loaded
-# before publishing so deployApp() knows what is necessary.
-#source("./R/util.R")
-
 # Set the account info for deployment.
 setAccountInfo(name   = Sys.getenv("shinyapps_name"),
                token  = Sys.getenv("shinyapps_token"),
