7e000ba640d410060ae997de8aa5e472fdc1a6c6
ischerr <iainscherr@gmail.com>
2019-12-03 18:57:08 +0000

    rgdal packrat

--- deploy.r
+++ deploy.r
@@ -1,4 +1,8 @@
 library(rsconnect)
+
+# Print a list of app dependencies. Libraries need to be loaded
+# before publishing so deployApp() knows what is necessary.
+source("Packages.R")
 # Set the account info for deployment.
 setAccountInfo(name   = Sys.getenv("shinyapps_name"),
                token  = Sys.getenv("shinyapps_token"),
