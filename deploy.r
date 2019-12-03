939e4acb8c760c72a6ad43ab2be370db5183b663
ischerr <iainscherr@gmail.com>
2019-12-03 09:24:07 +0000

    Travis CI Prep
    Preparing GitHub files for automatic deployment

---
+++ deploy.r
@@ -0,0 +1,13 @@
+library(rsconnect)
+
+# Print a list of app dependencies. Libraries need to be loaded
+# before publishing so deployApp() knows what is necessary.
+source("./R/util.R")
+
+# Set the account info for deployment.
+setAccountInfo(name   = Sys.getenv("shinyapps_name"),
+               token  = Sys.getenv("shinyapps_token"),
+               secret = Sys.getenv("shinyapps_secret"))
+
+# Deploy the application.
+deployApp()
