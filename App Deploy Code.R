d4892fe1a0f0914890e3da01e3e625c814216a41
ischerr <iainscherr@gmail.com>
2019-12-03 09:03:14 +0000

    Delete App Deploy Code.R

--- App Deploy Code.R
+++
@@ -1,7 +0,0 @@
-setwd("J:/ENERGY BRANCH/Statistics/R Shiny/Reorganised")
-
-rsconnect::setAccountInfo(name='ischerr', token='9739438703127EB0D7884424EC7E0483', secret='1mmueOj5gt2HsvEJsJUeyEK+Ik5AyAwRuUHNwjiM')
-
-rsconnect::deployApp(appName = "sg-scottish-energy-statistics",
-                     account = "ischerr")
-
