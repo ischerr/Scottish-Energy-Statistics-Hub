917238f5d23f597a9e6987ee710fe9b5c9494301
ischerr <iainscherr@gmail.com>
2019-12-03 10:04:51 +0000

    Travis fixing

--- deploy.r
+++ deploy.r
@@ -2,7 +2,7 @@ library(rsconnect)
 
 # Print a list of app dependencies. Libraries need to be loaded
 # before publishing so deployApp() knows what is necessary.
-source("./R/util.R")
+#source("./R/util.R")
 
 # Set the account info for deployment.
 setAccountInfo(name   = Sys.getenv("shinyapps_name"),
