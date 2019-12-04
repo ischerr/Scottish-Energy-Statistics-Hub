d96ba24bbda39cdf6afe6ddd674493e136f0ec54
ischerr <iainscherr@gmail.com>
2019-12-04 09:39:18 +0000

    pacman test

--- packages.R
+++ packages.R
@@ -1,46 +1,47 @@
-library(shiny)                # For the app iteslf.
-library(shinythemes)          # For the "cerulean" theme.
-library(shinyhelper)          # Used for help modal boxes.
-library(shinyEffects)         # Used for home page effects.
-library(shinyanimate)         # Used for home page effects.
-library(shinycssloaders)      # For loaders.
-library(shinyjs)              # Various UI effects
-library(magrittr)             # Dependency of other packages. Lets use the pipe operator (%>%).
-library(data.table)           # Needed for function na.omit().
-library(DT)                   # For the interactive tables.
-library(dygraphs)             # For the interactive graphs.
-library(leaflet)              # For interactive maps.
-library(RColorBrewer)         # Used to create a colour palette for the map.
-library(rgdal)                # Needed to load shapfile for the map.
-library(plyr)                 # Needed for function revalue(), for editing country names.
-library(rmarkdown)            # Used for reports, especially important is function render().
-library(knitr)                # Takes Rmd. file and turns it into .md document which includes the R code and its output.
-library(networkD3)            # Used to create the sankey diagram.
-library(treemap)              # Used to create the static treemap.
-library(dplyr)                # Used for data manipulation.
-library(ggplot2)              # Used for plots in country and sector profiles.
-library(leaflet.minicharts)   # Used to superimpose piecharts on a leaflet map.
-library(plotly)               # Used for the streamgraph.
-library(ggsci)                # Used for colour palettes.
-library(ggflags)              # Expands ggplot with new geom for adding flags.
-library(countrycode)          # Enables conversion from Common country names to ISO codes.
-library(knitr)                # Used for sector definitions table
-library(kableExtra)           # Used for styling the sector definitions table
-library(extrafont)            # Extra fonts
-library(readtext)
-library(htmlwidgets)
-library(readxl)
-library(gganimate)
-library(gifski)
-library(readr)
-library("maptools")
-library(tmaptools)
-library(tmap)
-library("sf")
-library("leaflet")
-library("rgeos")
-library(shinyWidgets)
-library(readxl)
-library(lubridate)
-library(ggrepel)
-library(raceland)
+if (!require("pacman")) install.packages("pacman"); library(pacman)
+p_load("shiny")                # For the app iteslf.
+p_load("shinythemes")          # For the "cerulean" theme.
+p_load("shinyhelper")          # Used for help modal boxes.
+p_load("shinyEffects")         # Used for home page effects.
+p_load("shinyanimate")         # Used for home page effects.
+p_load("shinycssloaders")      # For loaders.
+p_load("shinyjs")              # Various UI effects
+p_load("magrittr")             # Dependency of other packages. Lets use the pipe operator (%>%").
+p_load("data.table")           # Needed for function na.omit(").
+p_load("DT")                   # For the interactive tables.
+p_load("dygraphs")             # For the interactive graphs.
+p_load("leaflet")              # For interactive maps.
+p_load("RColorBrewer")         # Used to create a colour palette for the map.
+p_load("rgdal")                # Needed to load shapfile for the map.
+p_load("plyr")                 # Needed for function revalue("), for editing country names.
+p_load("rmarkdown")            # Used for reports, especially important is function render(").
+p_load("knitr")                # Takes Rmd. file and turns it into .md document which includes the R code and its output.
+p_load("networkD3")            # Used to create the sankey diagram.
+p_load("treemap")              # Used to create the static treemap.
+p_load("dplyr")                # Used for data manipulation.
+p_load("ggplot2")              # Used for plots in country and sector profiles.
+p_load("leaflet.minicharts")   # Used to superimpose piecharts on a leaflet map.
+p_load("plotly")               # Used for the streamgraph.
+p_load("ggsci")                # Used for colour palettes.
+p_load("ggflags")              # Expands ggplot with new geom for adding flags.
+p_load("countrycode")          # Enables conversion from Common country names to ISO codes.
+p_load("knitr")                # Used for sector definitions table
+p_load("kableExtra")           # Used for styling the sector definitions table
+p_load("extrafont")            # Extra fonts
+p_load("readtext")
+p_load("htmlwidgets")
+p_load("readxl")
+p_load("gganimate")
+p_load("gifski")
+p_load("readr")
+p_load("maptools")
+p_load("tmaptools")
+p_load("tmap")
+p_load("sf")
+p_load("leaflet")
+p_load("rgeos")
+p_load("shinyWidgets")
+p_load("readxl")
+p_load("lubridate")
+p_load("ggrepel")
+p_load("raceland")
