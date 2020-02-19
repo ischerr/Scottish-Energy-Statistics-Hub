0f2a912c5170016dd80fa326a9efc11bc47cc663
ischerr <iainscherr@gmail.com>
2020-02-19 13:13:20 +0000

    Update deploy.r

--- deploy.r
+++ deploy.r
@@ -7,14 +7,18 @@ setAccountInfo(name   = Sys.getenv("shinyapps_name"),
                secret = Sys.getenv("shinyapps_secret"))
 
 # Deploy the application.
-deployApp(appName = "Reorganised", lint = TRUE)
+deployApp(appName = "Reorganised",
+          account = Sys.getenv("shinyapps_name"),
+          lint = TRUE)
 
 
 setAccountInfo(name   = Sys.getenv("shinyapps_name2"),
                token  = Sys.getenv("shinyapps_token2"),
                secret = Sys.getenv("shinyapps_secret2"))
 
-deployApp(appName = "Energy", lint = TRUE)
+deployApp(appName = "Energy",
+          account = Sys.getenv("shinyapps_name2"),
+          lint = TRUE)
 
 deployApp(appName = "sg-scottish-energy-statistics", lint = TRUE)
 
