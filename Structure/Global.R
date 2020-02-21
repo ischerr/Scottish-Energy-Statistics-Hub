f94b318a059a6b3dd75ed8a3c5bd3fbb18f50cc5
ischerr <iainscherr@gmail.com>
2020-02-21 10:44:20 +0000

    Update Global.R

--- Structure/Global.R
+++ Structure/Global.R
@@ -2,7 +2,7 @@ Sources <- read_excel("Structure/Sources.xlsx")
 
 SourceLookup <- function(x){
   tagList(
-    a(Sources[which(Sources$Code == x),][2], href = Sources[which(Sources$Code == x),][3]),
+    a(Sources[which(Sources$Code == x),][2], href = Sources[which(Sources$Code == x),][3], target="_blank"),
     br()
   )
 }
