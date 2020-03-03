620714142f84e7d4c9cd395b29517cdd62693bda
ischerr <iainscherr@gmail.com>
2020-03-03 11:03:03 +0000

    Update EnSupplySwitch.R

--- Structure/5 - Consumers/EnSupplySwitch.R
+++ Structure/5 - Consumers/EnSupplySwitch.R
@@ -106,6 +106,10 @@ EnSupplySwitch <- function(input, output, session) {
   
   MarketSwitch <- Data  
   
+  ChartColours <- c("#4292c6", "#7bccc4", "#08519c", "#ef3b2c")
+  sourcecaption = "Source: BEIS"
+  plottitle = "Proportion of payment methods used\nfor electricity bills"
+  
   ### From ESD ###
   
   output$EnSupplySwitchSubtitle <- renderText({
@@ -115,9 +119,7 @@ EnSupplySwitch <- function(input, output, session) {
   
   output$EnSupplySwitchPlot <- renderPlotly({
     
-    ChartColours <- c("#4292c6", "#7bccc4", "#08519c", "#ef3b2c")
-    sourcecaption = "Source: BEIS"
-    plottitle = "Proportion of payment methods used\nfor electricity bills"
+
     
     
     
@@ -289,14 +291,14 @@ EnSupplySwitch <- function(input, output, session) {
   
   output$EnSupplySwitchTable = renderDataTable({
     
-    EnSupplySwitch <- read_excel(
-      "Structure/CurrentWorking.xlsx",
-      sheet = "Switching by LA",
-      skip = 13
-    )[2:4]
+  MarketSwitchTable <- MarketSwitch  
+    
+  names(MarketSwitchTable)[1] <- "Month"
+  
+  MarketSwitchTable$Month <- as.character(format(MarketSwitchTable$Month, "%B %Y"))
 
     datatable(
-      EnSupplySwitch,
+      MarketSwitchTable,
       extensions = 'Buttons',
       
       rownames = FALSE,
@@ -307,17 +309,17 @@ EnSupplySwitch <- function(input, output, session) {
         fixedColumns = FALSE,
         autoWidth = TRUE,
         ordering = TRUE,
-        title = "Proportion of customers who have switched energy supplier by local authority, 2018",
+        title = "Proportion of customers who have switched energy supplier",
         dom = 'ltBp',
         buttons = list(
           list(extend = 'copy'),
           list(
             extend = 'excel',
-            title = 'Proportion of customers who have switched energy supplier by local authority, 2018',
+            title = 'Proportion of customers who have switched energy supplier',
             header = TRUE
           ),
           list(extend = 'csv',
-               title = 'Proportion of customers who have switched energy supplier by local authority, 2018')
+               title = 'Proportion of customers who have switched energy supplier')
         ),
         
         # customize the length menu
@@ -327,63 +329,8 @@ EnSupplySwitch <- function(input, output, session) {
         pageLength = 10
       )
     ) %>%
-      formatPercentage(c(3), 1) 
+      formatPercentage(c(2:5), 1)
   })
-  
-  output$EnSupplySwitchTimeSeriesTable = renderDataTable({
-    
-    EnSupplySwitch <- read_excel(
-      "Structure/CurrentWorking.xlsx",
-      sheet = "Non-home supplier elec",
-      skip = 13
-    )
-    
-    names(EnSupplySwitch)[1] <- "Quarter"#
-    
-    EnSupplySwitch <- EnSupplySwitch[complete.cases(EnSupplySwitch),]
-    
-    EnSupplySwitch$Quarter <- as.Date(as.numeric(EnSupplySwitch$Quarter), origin = "1899-12-30")
-    
-    EnSupplySwitch$Quarter <- as.character(as.yearqtr(EnSupplySwitch$Quarter))
-    
-    
-    datatable(
-      EnSupplySwitch,
-      extensions = 'Buttons',
-      
-      rownames = FALSE,
-      options = list(
-        paging = TRUE,
-        pageLength = -1,
-        searching = TRUE,
-        fixedColumns = FALSE,
-        autoWidth = TRUE,
-        ordering = TRUE,
-        order = list(list(0, 'desc')),
-        title = "Proportion of households not on the gas grid by local authority (estimates), Scotland, 2017",
-        dom = 'ltBp',
-        buttons = list(
-          list(extend = 'copy'),
-          list(
-            extend = 'excel',
-            title = 'Proportion of households not on the gas grid by local authority (estimates), Scotland, 2017',
-            header = TRUE
-          ),
-          list(extend = 'csv',
-               title = 'Proportion of households not on the gas grid by local authority (estimates), Scotland, 2017')
-        ),
-        
-        # customize the length menu
-        lengthMenu = list( c(10, 20, -1) # declare values
-                           , c(10, 20, "All") # declare titles
-        ), # end of lengthMenu customization
-        pageLength = 10
-      )
-    ) %>%
-      formatPercentage(2:9, 1)
-  })
-  
-  
   
   output$Text <- renderUI({
     tagList(column(12,
@@ -398,11 +345,6 @@ EnSupplySwitch <- function(input, output, session) {
     toggle("EnSupplySwitchTable")
   })
   
-  observeEvent(input$ToggleTable2, {
-    toggle("EnSupplySwitchTimeSeriesTable")
-  })
-  
-
   
   observeEvent(input$ToggleText, {
     toggle("Text")
@@ -412,7 +354,265 @@ EnSupplySwitch <- function(input, output, session) {
   output$EnSupplySwitch.png <- downloadHandler(
     filename = "EnSupplySwitch.png",
     content = function(file) {
-      writePNG(readPNG("Structure/5 - Consumers/EnSupplySwitchChart.png"), file) 
+      
+      Data <- read_csv("Processed Data/Output/Consumers/MarketSwitching.csv")
+      
+      names(Data)[1] <- "Year"
+      
+      MarketSwitch <- Data  
+      
+      ChartColours <- c("#4292c6", "#7bccc4", "#08519c", "#ef3b2c")
+      sourcecaption = "Source: BEIS"
+      plottitle = "Proportion of payment methods used\nfor electricity bills"
+      
+      ChartWidth <- (max(MarketSwitch$Year) - min(MarketSwitch$Year))
+      
+      MarketSwitch$Year <- as.Date(MarketSwitch$Year, format = "%d/%m/%Y")  
+      
+      MarketSwitchChart <- MarketSwitch %>%
+        ggplot(aes(x = Year), family = "Century Gothic") +
+        geom_line(
+          aes(y = `GB`,
+              label = paste0(`GB` * 100, "%")),
+          colour = ChartColours[4],
+          size = 1.5,
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year - ChartWidth*0.055,
+            y = `GB`,
+            label = ifelse(Year == min(Year), percent(`GB`, accuracy = .1), ""),
+            hjust = 0.5,
+            fontface = 2
+          ),
+          colour = ChartColours[4],
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year + ChartWidth*0.070,
+            y = `GB`,
+            label = ifelse(Year == max(Year), percent(`GB`,  accuracy = .1), ""),
+            hjust = 0.5,
+            fontface = 2
+          ),
+          colour = ChartColours[4],
+          family = "Century Gothic"
+        ) +
+        geom_point(
+          data = tail(MarketSwitch, 1),
+          aes(x = Year,
+              y = `GB`,
+              show_guide = FALSE),
+          size = 4,
+          colour = ChartColours[4],
+          family = "Century Gothic"
+        ) +
+        annotate(
+          "text",
+          x = mean(MarketSwitch$Year),
+          y = mean(MarketSwitch$`GB`),
+          label = "GB",
+          hjust = 0.5,
+          vjust = 1.65,
+          colour = ChartColours[4],
+          fontface = 2,
+          family = "Century Gothic"
+        ) +
+        geom_line(
+          aes(y = `North Scotland`,
+              label = percent(`North Scotland`)),
+          colour = ChartColours[2],
+          size = 1.5,
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year - ChartWidth*0.055,
+            y = `North Scotland`,
+            label = ifelse(Year == min(Year), percent(`North Scotland`, accuracy = .1), ""),
+            hjust = 0.5,
+            fontface = 2
+          ),
+          colour = ChartColours[2],
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year + ChartWidth*0.070,
+            y = `North Scotland`,
+            label = ifelse(Year == max(Year), percent(`North Scotland`, accuracy = .1), ""),
+            hjust = 0.5,
+            vjust = .5,
+            fontface = 2
+          ),
+          colour = ChartColours[2],
+          family = "Century Gothic"
+        ) +
+        geom_point(
+          data = tail(MarketSwitch, 1),
+          aes(x = Year,
+              y = `North Scotland`,
+              show_guide = FALSE),
+          colour = ChartColours[2],
+          size = 4,
+          family = "Century Gothic"
+        ) +
+        annotate(
+          "text",
+          x = mean(MarketSwitch$Year),
+          y = mean(MarketSwitch$`North Scotland`),
+          label = "North Scotland",
+          hjust = 0.5,
+          vjust = 4,
+          colour = ChartColours[2],
+          fontface = 2,
+          family = "Century Gothic"
+        ) +
+        geom_line(
+          aes(y = `South Scotland`,
+              label = paste0(`South Scotland` * 100, "%")),
+          colour = ChartColours[3],
+          size = 1.5,
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year - ChartWidth*0.055,
+            y = `South Scotland`,
+            label = ifelse(Year == min(Year), percent(`South Scotland`, accuracy = .1), ""),
+            hjust = 0.5,
+            fontface = 2
+          ),
+          colour = ChartColours[3],
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year + ChartWidth*0.070,
+            y = `South Scotland`,
+            label = ifelse(Year == max(Year), percent(`South Scotland`, accuracy = .1), ""),
+            hjust = 0.5,
+            vjust = .5,
+            fontface = 2
+          ),
+          colour = ChartColours[3],
+          family = "Century Gothic"
+        ) +
+        geom_point(
+          data = tail(MarketSwitch, 1),
+          aes(x = Year,
+              y = `South Scotland`,
+              show_guide = FALSE),
+          colour = ChartColours[3],
+          size = 4,
+          family = "Century Gothic"
+        ) +
+        annotate(
+          "text",
+          x = mean(MarketSwitch$Year),
+          y = mean(MarketSwitch$`South Scotland`),
+          label = "South Scotland",
+          hjust = 0.5,
+          vjust = -1.2,
+          colour = ChartColours[3],
+          fontface = 2,
+          family = "Century Gothic"
+        ) +
+        geom_line(
+          aes(y = `Whole Scotland`,
+              label = paste0(`Whole Scotland` * 100, "%")),
+          colour = ChartColours[1],
+          size = 1.5,
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year - ChartWidth*0.055,
+            y = `Whole Scotland`,
+            label = ifelse(Year == min(Year), percent(`Whole Scotland`, accuracy = .1), ""),
+            hjust = 0.5,
+            fontface = 2
+          ),
+          colour = ChartColours[1],
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year + ChartWidth*0.070,
+            y = `Whole Scotland`,
+            label = ifelse(Year == max(Year), percent(`Whole Scotland`,  accuracy = .1), ""),
+            hjust = 0.5,
+            vjust = 1.4,
+            fontface = 2
+          ),
+          colour = ChartColours[1],
+          family = "Century Gothic"
+        ) +
+        geom_point(
+          data = tail(MarketSwitch, 1),
+          aes(x = Year,
+              y = `Whole Scotland`,
+              
+              show_guide = FALSE),
+          size = 4,
+          colour = ChartColours[1],
+          family = "Century Gothic"
+        ) +
+        annotate(
+          "text",
+          x = mean(MarketSwitch$Year),
+          y = mean(MarketSwitch$`Whole Scotland`),
+          label = "Whole Scotland",
+          hjust = 0.5,
+          vjust = 3,
+          colour = ChartColours[1],
+          fontface = 2,
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year,
+            y = 0,
+            label = ifelse(
+              Year == max(Year) |
+                Year == min(Year),
+              format(Year, format = "%B %Y"),
+              ""
+            ),
+            hjust = 0.5,
+            vjust = 1.5,
+            fontface = 2
+          ),
+          colour = ChartColours[1],
+          family = "Century Gothic"
+        )
+      
+      MarketSwitchChart
+      
+      MarketSwitchChart <-
+        DailyChart(MarketSwitchChart,
+                   MarketSwitch,
+                   plottitle,
+                   sourcecaption,
+                   ChartColours)
+      
+      
+      MarketSwitchChart <- MarketSwitchChart +
+        coord_cartesian(xlim = c(min(MarketSwitch$Year)-(max(MarketSwitch$Year) - min(MarketSwitch$Year))*0.05, max(MarketSwitch$Year)+(max(MarketSwitch$Year) - min(MarketSwitch$Year))*0.1))
+      
+      MarketSwitchChart
+      
+      ggsave(
+        file,
+        plot =  MarketSwitchChart,
+        width = 14,
+        height = 14,
+        units = "cm",
+        dpi = 300
+      )
+      
     }
   )
 }
