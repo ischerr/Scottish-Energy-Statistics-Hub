c003c52241dc51d614c340feecd9c350a5232106
ischerr <iainscherr@gmail.com>
2020-07-10 13:31:52 +0100

    Corrections

--- Structure/6 - System Security/ElecStorage.R
+++ Structure/6 - System Security/ElecStorage.R
@@ -644,6 +644,17 @@ ElecStorage <- function(input, output, session) {
     filename = "ElecStorageCap.png",
     content = function(file) {
       
+      Data <- read_excel("Structure/CurrentWorking.xlsx", 
+                         sheet = "Renewable elec pipeline", col_names = TRUE,
+                         skip = 26, n_max = 1)
+      Quarter <- substr(Data[1,1], 8,8)
+      
+      Quarter <- as.numeric(Quarter)*3
+      
+      Year <- substr(Data[1,1], 1,4)
+      
+      paste("Scotland,", month.name[Quarter], Year)
+      
       ### Load Packages and Functions
       Data <- read_excel("Structure/CurrentWorking.xlsx",
                          sheet = "Electricity storage", skip = 14)[1:2]
@@ -752,7 +763,7 @@ ElecStorage <- function(input, output, session) {
       
       EnergyStorageTechChart <-
         EnergyStorageTechChart +
-        labs(subtitle = "Scotland, September 2019") +
+        labs(subtitle = paste("Scotland,", month.name[Quarter], Year)) +
         ylim(-150, max(EnergyStorageTech$top))+
         coord_flip()
       
