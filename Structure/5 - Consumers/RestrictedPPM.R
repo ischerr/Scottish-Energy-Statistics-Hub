0ffd3499a2ad115b3d8f1afb40a483e2b6fd3648
ischerr <iainscherr@gmail.com>
2020-08-14 13:20:38 +0100

    Update RestrictedPPM.R

--- Structure/5 - Consumers/RestrictedPPM.R
+++ Structure/5 - Consumers/RestrictedPPM.R
@@ -11,10 +11,10 @@ RestrictedPPMOutput <- function(id) {
   ns <- NS(id)
   tagList(
     tabsetPanel(
-    tabPanel("Economy 7",
+    tabPanel("Economy 7 meter proportion by LA",
     fluidRow(column(8,
                     h3("Proportion of Economy 7 meters by local authority", style = "color: #68c3ea;  font-weight:bold"),
-                    h4(textOutput(ns('RestrictedMeterSubtitle')), style = "color: #68c3ea;")
+                    h4(textOutput(ns('RestrictedMeterPropSubtitle')), style = "color: #68c3ea;")
     ),
              column(
                4, style = 'padding:15px;',
@@ -23,22 +23,37 @@ RestrictedPPMOutput <- function(id) {
     
     tags$hr(style = "height:3px;border:none;color:#68c3ea;background-color:#68c3ea;"),
     #imageOutput(ns("RestrictedMeterPlot"), height = "700px")%>% withSpinner(color="#68c3ea"),
-    leafletOutput(ns("RestrictedMeterMap"), height = "800px")%>% withSpinner(color="#68c3ea"),
+    leafletOutput(ns("RestrictedMeterPropMap"), height = "800px")%>% withSpinner(color="#68c3ea"),
     tags$hr(style = "height:3px;border:none;color:#68c3ea;background-color:#68c3ea;")),
-    tabPanel("Time Series",
+        tabPanel("Economy 7 meter proportion",
              fluidRow(column(8,
-                             h3("Proportion of restricted meters in Scotland", style = "color: #68c3ea;  font-weight:bold"),
-                             h4(textOutput(ns('RestrictedMetersPropSubtitle')), style = "color: #68c3ea;")
+                             h3("Proportion of Economy 7 meters in Scotland", style = "color: #68c3ea;  font-weight:bold"),
+                             h4(textOutput(ns('RestrictedMetersPropTSSubtitle')), style = "color: #68c3ea;")
              ),
              column(
                4, style = 'padding:15px;',
-               downloadButton(ns('RestrictedMetersProp.png'), 'Download Graph', style="float:right")
+               downloadButton(ns('RestrictedMetersPropTS.png'), 'Download Graph', style="float:right")
              )),
              
              tags$hr(style = "height:3px;border:none;color:#68c3ea;background-color:#68c3ea;"),
              #dygraphOutput(ns("RestrictedMeterPlot")),
-             plotlyOutput(ns("RestrictedMetersPropPlot"))%>% withSpinner(color="#68c3ea"),
-             tags$hr(style = "height:3px;border:none;color:#68c3ea;background-color:#68c3ea;"))),
+             plotlyOutput(ns("RestrictedMetersPropTSPlot"))%>% withSpinner(color="#68c3ea"),
+             tags$hr(style = "height:3px;border:none;color:#68c3ea;background-color:#68c3ea;")),
+    tabPanel("Economy 7 meter amount",
+             fluidRow(column(8,
+                             h3("Proportion of Economy 7 meters in Scotland", style = "color: #68c3ea;  font-weight:bold"),
+                             h4(textOutput(ns('RestrictedMetersTSSubtitle')), style = "color: #68c3ea;")
+             ),
+             column(
+               4, style = 'padding:15px;',
+               downloadButton(ns('RestrictedMetersTS.png'), 'Download Graph', style="float:right")
+             )),
+             
+             tags$hr(style = "height:3px;border:none;color:#68c3ea;background-color:#68c3ea;"),
+             #dygraphOutput(ns("RestrictedMeterPlot")),
+             plotlyOutput(ns("RestrictedMetersTSPlot"))%>% withSpinner(color="#68c3ea"),
+             tags$hr(style = "height:3px;border:none;color:#68c3ea;background-color:#68c3ea;"))
+),
    
     fluidRow(
     column(10,h3("Commentary", style = "color: #68c3ea;  font-weight:bold")),
@@ -49,14 +64,22 @@ RestrictedPPMOutput <- function(id) {
     ),
     tags$hr(style = "height:3px;border:none;color:#68c3ea;background-color:#68c3ea;"),
     tabsetPanel(
-      tabPanel("Economy 7",
+      tabPanel("Economy 7 proportion",
     fluidRow(
     column(10, h3("Data - Proportion of Economy 7 meters by local authority", style = "color: #68c3ea;  font-weight:bold")),
     column(2, style = "padding:15px",  actionButton(ns("ToggleTable"), "Show/Hide Table", style = "float:right; "))
     ),
     fluidRow(
-      column(12, dataTableOutput(ns("RestrictedMeterTable"))%>% withSpinner(color="#68c3ea"))),
-    tags$hr(style = "height:3px;border:none;color:#68c3ea;background-color:#68c3ea;"))),
+      column(12, dataTableOutput(ns("RestrictedMeterPropTable"))%>% withSpinner(color="#68c3ea"))),
+    tags$hr(style = "height:3px;border:none;color:#68c3ea;background-color:#68c3ea;")),
+    tabPanel("Economy 7 amount",
+             fluidRow(
+               column(10, h3("Data - Amount of Economy 7 meters by local authority", style = "color: #68c3ea;  font-weight:bold")),
+               column(2, style = "padding:15px",  actionButton(ns("ToggleTable2"), "Show/Hide Table", style = "float:right; "))
+             ),
+             fluidRow(
+               column(12, dataTableOutput(ns("RestrictedMeterAmountTable"))%>% withSpinner(color="#68c3ea"))),
+             tags$hr(style = "height:3px;border:none;color:#68c3ea;background-color:#68c3ea;"))),
     fluidRow(
       column(2, p("Update expected:")),
       column(2,
@@ -86,30 +109,13 @@ RestrictedPPM <- function(input, output, session) {
   
   ### From ESD ###
   
-  output$RestrictedMeterSubtitle <- renderText({
+  output$RestrictedMeterPropSubtitle <- renderText({
     
     paste("Scotland, 2018")
   
     })
   
-  output$RestrictedMeterPlot <- renderImage({
-    
-    # A temp file to save the output. It will be deleted after renderImage
-    # sends it, because deleteFile=TRUE.
-    outfile <- tempfile(fileext='.png')
-   
-     writePNG(readPNG("Structure/5 - Consumers/RestrictedMeterOutput.png"),outfile) 
-    
-    # Generate a png
-    
-    
-    # Return a list
-    list(src = outfile,
-         alt = "This is alternate text")
-  }, deleteFile = TRUE)
-  
-  
-  output$RestrictedMeterTable = renderDataTable({
+  output$RestrictedMeterPropTable = renderDataTable({
     
     RestrictedMeter <- read_delim("Processed Data/Output/Restricted Meters/RestrictedMetersProp.txt", 
                                        "\t", escape_double = FALSE, trim_ws = TRUE)
@@ -154,18 +160,18 @@ RestrictedPPM <- function(input, output, session) {
       formatPercentage(2:100, 1)
   })
   
-  output$PrepaymentMeterTable = renderDataTable({
+  output$RestrictedMeterAmountTable = renderDataTable({
     
-    RestrictedMeter <- read_excel(
-      "Structure/CurrentWorking.xlsx",
-      sheet = "Restricted and PPM",
-      skip = 15
-    )[c(1,2,6,7)]
+    RestrictedMeter <- read_delim("Processed Data/Output/Restricted Meters/RestrictedMeters.txt", 
+                                  "\t", escape_double = FALSE, trim_ws = TRUE)
     
-    names(RestrictedMeter) <- c("LA Code", "Local Authority", "Pre-payment Meters", "Proportion of total meters")
+    names(RestrictedMeter) <- names(read_delim("Processed Data/Output/Restricted Meters/RestrictedMeters.txt", 
+                                               "\t", escape_double = FALSE, trim_ws = TRUE))
+    
+    RestrictedMeter[34,1] <- "All Scotland"
     
     datatable(
-      RestrictedMeter[c(2,1,3,4)],
+      RestrictedMeter[-33,],
       extensions = 'Buttons',
       
       rownames = FALSE,
@@ -176,17 +182,17 @@ RestrictedPPM <- function(input, output, session) {
         fixedColumns = FALSE,
         autoWidth = TRUE,
         ordering = TRUE,
-        title = "Economy 7 meters by Local Authority",
+        title = "Amount of Economy 7 meters by local authority",
         dom = 'ltBp',
         buttons = list(
           list(extend = 'copy'),
           list(
             extend = 'excel',
-            title = 'Economy 7 meters by Local Authority',
+            title = 'Amount of Economy 7 meters by local authority',
             header = TRUE
           ),
           list(extend = 'csv',
-               title = 'Economy 7 meters by Local Authority')
+               title = 'Amount of Economy 7 meters by local authority')
         ),
         
         # customize the length menu
@@ -196,12 +202,9 @@ RestrictedPPM <- function(input, output, session) {
         pageLength = 10
       )
     ) %>%
-      formatPercentage(4, 1) %>% 
-      formatRound(3, 0)
+      formatRound(2:100, 0)
   })
-  
-  
-  
+
   output$Text <- renderUI({
     tagList(column(12,
                    HTML(
@@ -210,21 +213,17 @@ RestrictedPPM <- function(input, output, session) {
                    )))
   })
   
-  
   observeEvent(input$ToggleTable, {
-    toggle("RestrictedMeterTable")
+    toggle("RestrictedMeterPropTable")
   })
   
   observeEvent(input$ToggleTable2, {
-    toggle("PrepaymentMeterTable")
+    toggle("RestrictedMeterAmountTable")
   })
-  
-
   
   observeEvent(input$ToggleText, {
     toggle("Text")
   })
-  
   
   output$RestrictedMeter.png <- downloadHandler(
     filename = "RestrictedMeter.png",
@@ -233,36 +232,7 @@ RestrictedPPM <- function(input, output, session) {
     }
   )
   
-  output$PPMMeterSubtitle <- renderText({
-    
-    paste("Scotland, 2017")
-    
-  })
-  
-  output$PPMMeterPlot <- renderImage({
-    
-    # A temp file to save the output. It will be deleted after renderImage
-    # sends it, because deleteFile=TRUE.
-    outfile <- tempfile(fileext='.png')
-    
-    writePNG(readPNG("Structure/5 - Consumers/PPMMeterOutput.png"),outfile) 
-    
-    # Generate a png
-    
-    
-    # Return a list
-    list(src = outfile,
-         alt = "This is alternate text")
-  }, deleteFile = TRUE)
-  
-  output$PPMMeter.png <- downloadHandler(
-    filename = "PPMMeter.png",
-    content = function(file) {
-      writePNG(readPNG("Structure/5 - Consumers/PPMMeterChart.png"), file) 
-    }
-  )
-  
-  output$RestrictedMetersPropPlot <- renderPlotly  ({
+  output$RestrictedMetersPropTSPlot <- renderPlotly  ({
     
     RestrictedMetersProp <- read_delim("Processed Data/Output/Restricted Meters/RestrictedMetersProp.txt", 
                                        "\t", escape_double = FALSE, col_names = FALSE, 
@@ -352,7 +322,7 @@ RestrictedPPM <- function(input, output, session) {
     
   })
   
-  output$RestrictedMetersProp.png <- downloadHandler(
+  output$RestrictedMetersPropTS.png <- downloadHandler(
     filename = "RestrictedMetersProp.png",
     content = function(file) {
       
@@ -374,7 +344,7 @@ RestrictedPPM <- function(input, output, session) {
       RestrictedMetersProp$Proportion <- as.numeric(as.character(RestrictedMetersProp$Proportion))
       
       plottitle <-
-        "Proportion of Economy 7 meters in Scotland"
+        "Proportion of Economy 7 meters\nin Scotland"
       sourcecaption <- "Source: BEIS"
       
       
@@ -466,7 +436,7 @@ RestrictedPPM <- function(input, output, session) {
       
 })
   
-  output$RestrictedMetersPropSubtitle <- renderText({
+  output$RestrictedMetersPropTSSubtitle <- renderText({
     
     RestrictedMetersProp <- read_delim("Processed Data/Output/Restricted Meters/RestrictedMetersProp.txt", 
                                        "\t", escape_double = FALSE, col_names = FALSE, 
@@ -485,7 +455,7 @@ RestrictedPPM <- function(input, output, session) {
     paste0("Scotland, ", min(RestrictedMetersProp$Year)," - ",  max(RestrictedMetersProp$Year))
   })
   
-  output$RestrictedMeterMap <- renderLeaflet({
+  output$RestrictedMeterPropMap <- renderLeaflet({
     
     ### Load Packages
     library(readr)
@@ -566,6 +536,232 @@ RestrictedPPM <- function(input, output, session) {
       ) 
     
   }) 
+  
+  output$RestrictedMetersTSPlot <- renderPlotly  ({
+    
+    RestrictedMetersProp <- read_delim("Processed Data/Output/Restricted Meters/RestrictedMeters.txt", 
+                                       "\t", escape_double = FALSE, col_names = FALSE, 
+                                       trim_ws = TRUE)
+    
+    
+    
+    RestrictedMetersProp <- as_tibble(t(RestrictedMetersProp[which(RestrictedMetersProp[2] == "LA Code" | RestrictedMetersProp[2] == "S92000003"),]))
+    
+    RestrictedMetersProp <- tail(RestrictedMetersProp, -2)
+    
+    names(RestrictedMetersProp) <- c("Year", "Proportion")
+    
+    RestrictedMetersProp$Year <- as.numeric(as.character(RestrictedMetersProp$Year))  
+    
+    RestrictedMetersProp$Proportion <- as.numeric(as.character(RestrictedMetersProp$Proportion))
+    
+    plottitle <-
+      "Coal Proportion (million tonnes)"
+    sourcecaption <- "Source: BEIS"
+    
+    ChartColours <- c("#68c3ea", "#66c2a5", "#fc8d62", "#8da0cb")
+    BarColours <-
+      c(    "#0868ac","#43a2ca","#7bccc4"
+      )
+    
+    p <-  plot_ly(RestrictedMetersProp,x = ~ Year ) %>% 
+      add_trace(data = RestrictedMetersProp,
+                x = ~ Year,
+                y = ~ Proportion,
+                name = "Amount",
+                type = 'scatter',
+                mode = 'lines',
+                legendgroup = "1",
+                text = paste0(
+                  "Amount: ",
+                  format(RestrictedMetersProp$Proportion, big.mark = ","),
+                  "\nYear: ",
+                  paste(RestrictedMetersProp$Year)
+                ),
+                hoverinfo = 'text',
+                line = list(width = 6, color = ChartColours[1], dash = "none")
+      )  %>% 
+      add_trace(
+        data = tail(RestrictedMetersProp[which(RestrictedMetersProp$Proportion > 0 | RestrictedMetersProp$Proportion < 0),], 1),
+        x = ~ Year,
+        y = ~ Proportion,
+        legendgroup = "1",
+        name = "Total",
+        text = paste0(
+          "Amount: ",
+          format(RestrictedMetersProp[which(RestrictedMetersProp$Proportion > 0 | RestrictedMetersProp$Proportion < 0),][-1,]$Proportion, big.mark = ","),
+          "\nYear: ",
+          paste(RestrictedMetersProp[which(RestrictedMetersProp$Proportion > 0 | RestrictedMetersProp$Proportion < 0),][-1,]$Year)
+        ),
+        hoverinfo = 'text',
+        showlegend = FALSE ,
+        type = "scatter",
+        mode = 'markers',
+        marker = list(size = 18, 
+                      color = ChartColours[1])
+      )  %>%  
+      layout(
+        barmode = 'stack',
+        bargap = 0.66,
+        legend = list(font = list(color = "#68c3ea"),
+                      orientation = 'h'),
+        hoverlabel = list(font = list(color = "white"),
+                          hovername = 'text'),
+        hovername = 'text',
+        
+        xaxis = list(title = "",
+                     showgrid = FALSE),
+        yaxis = list(
+          title = "",
+          tickformat = "",
+          showgrid = TRUE,
+          zeroline = TRUE,
+          zerolinecolor = ChartColours[1],
+          zerolinewidth = 2,
+          rangemode = "tozero"
+        )
+      ) %>% 
+      config(displayModeBar = F)
+    p
+    
+    
+  })
+  
+  output$RestrictedMetersTS.png <- downloadHandler(
+    filename = "RestrictedMetersTS.png",
+    content = function(file) {
+      
+      
+      RestrictedMetersProp <- read_delim("Processed Data/Output/Restricted Meters/RestrictedMeters.txt", 
+                                         "\t", escape_double = FALSE, col_names = FALSE, 
+                                         trim_ws = TRUE)
+      
+      
+      
+      RestrictedMetersProp <- as_tibble(t(RestrictedMetersProp[which(RestrictedMetersProp[2] == "LA Code" | RestrictedMetersProp[2] == "S92000003"),]))
+      
+      RestrictedMetersProp <- tail(RestrictedMetersProp, -2)
+      
+      names(RestrictedMetersProp) <- c("Year", "Proportion")
+      
+      RestrictedMetersProp$Year <- as.numeric(as.character(RestrictedMetersProp$Year))  
+      
+      RestrictedMetersProp$Proportion <- as.numeric(as.character(RestrictedMetersProp$Proportion))
+      
+      length <- max(RestrictedMetersProp$Year)- min(RestrictedMetersProp$Year)
+      
+      plottitle <-
+        "Amount of Economy 7 meters\nin Scotland"
+      sourcecaption <- "Source: BEIS"
+      
+      
+      ChartColours <- c("#68c3ea", "#66c2a5", "#fc8d62", "#8da0cb")
+      BarColours <-
+        c(    "#0868ac","#43a2ca","#7bccc4"
+        )
+      
+      
+      RestrictedMetersPropChart <- RestrictedMetersProp %>%
+        ggplot(aes(x = Year), family = "Century Gothic") +
+        geom_line(
+          aes(
+            y = Proportion,
+            colour = ChartColours[2],
+            label = format(Proportion, big.mark = ",")
+          ),
+          size = 1.5,
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year,
+            y = Proportion,
+            label = ifelse(Year == min(Year), format(Proportion, big.mark = ","), ""),
+            hjust = 0.5,
+            vjust = 2.2,
+            colour = ChartColours[2],
+            fontface = 2
+          ),
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year,
+            y = Proportion,
+            label = ifelse(Year == max(Year), format(Proportion, big.mark = ","), ""),
+            hjust = 0.5,
+            vjust = -1,
+            colour = ChartColours[2],
+            fontface = 2
+          ),
+          family = "Century Gothic"
+        ) +
+        geom_point(
+          data = tail(RestrictedMetersProp, 1),
+          aes(
+            x = Year,
+            y = Proportion,
+            colour = ChartColours[2],
+            show_guide = FALSE
+          ),
+          size = 4,
+          family = "Century Gothic"
+        ) +
+        geom_text(
+          aes(
+            x = Year,
+            y = 0,
+            label = ifelse(Year == max(Year) |
+                             Year == min(Year), Year, ""),
+            hjust = 0.5,
+            vjust = 1.5,
+            fontface = 2
+          ),
+          colour = ChartColours[1],
+          family = "Century Gothic"
+        )
+      
+      RestrictedMetersPropChart <-
+        LinePercentChart(RestrictedMetersPropChart,
+                         RestrictedMetersProp,
+                         plottitle,
+                         sourcecaption,
+                         ChartColours)
+      
+      
+      RestrictedMetersPropChart <- RestrictedMetersPropChart +
+        xlim(min(RestrictedMetersProp$Year)-(length*0.08), max(RestrictedMetersProp$Year)+(length*0.08))
+      
+      ggsave(
+        file,
+        plot = RestrictedMetersPropChart,
+        width = 12.5,
+        height = 14,
+        units = "cm",
+        dpi = 300
+      )
+      
+      
+    })
+  
+  output$RestrictedMetersTSSubtitle <- renderText({
+    
+    RestrictedMetersProp <- read_delim("Processed Data/Output/Restricted Meters/RestrictedMeters.txt", 
+                                       "\t", escape_double = FALSE, col_names = FALSE, 
+                                       trim_ws = TRUE)
+    
+    
+    
+    RestrictedMetersProp <- as_tibble(t(RestrictedMetersProp[which(RestrictedMetersProp[2] == "LA Code" | RestrictedMetersProp[2] == "S92000003"),]))
+    
+    RestrictedMetersProp <- tail(RestrictedMetersProp, -2)
+    
+    names(RestrictedMetersProp) <- c("Year", "Proportion")
+    
+    RestrictedMetersProp$Year <- as.numeric(as.character(RestrictedMetersProp$Year))  
+    
+    paste0("Scotland, ", min(RestrictedMetersProp$Year)," - ",  max(RestrictedMetersProp$Year))
+  })
   
   
 }
