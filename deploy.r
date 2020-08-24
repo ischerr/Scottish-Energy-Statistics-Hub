6e783e4587afaa6c56cc37167fda332cf2a3f778
ischerr <iainscherr@gmail.com>
2020-08-24 11:20:00 +0100

    Update deploy.r

--- deploy.r
+++ deploy.r
@@ -1,6 +1,8 @@
 library(rsconnect)
 
 source("packages.R")
+
+
 # Set the account info for deployment.
 setAccountInfo(name   = Sys.getenv("shinyapps_name"),
                token  = Sys.getenv("shinyapps_token"),
