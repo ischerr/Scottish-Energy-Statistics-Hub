d4fd327c090445b148ec7bd2525284a58c7cc67b
ischerr <iainscherr@gmail.com>
2021-08-25 14:07:27 +0100

    Updates

--- Structure/1 - Whole System/EnProd.R
+++ Structure/1 - Whole System/EnProd.R
@@ -61,7 +61,7 @@ EnProdOutput <- function(id) {
       column(1, align = "right",
              HTML("<p><strong>Reason:</strong></p>")),
       column(7, align = "right", 
-             p("Quarterly Update - Energy Consumption")
+             p("Quarterly Update - GVA Figures")
     )),
     fluidRow(p(" ")),
     fluidRow(
